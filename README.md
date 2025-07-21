dac99d4a04be36b3afabb2fe75147c8c7213e263
fahmialis <fahmi@nusantara.technology>
2025-07-21 15:41:17 +0700

    add git and solved problems

---
+++ README.md
@@ -0,0 +1,14 @@
+🧾 How to Run
+
+1. Clone the Repository
+
+    `git clone https://github.com/your-username/abjad-converter-cli.git
+    cd abjad-converter-cli`
+
+2. Run the App
+
+    `node index.js`
+
+You will be prompted to enter a sentence. The app will then display the result of the transformation based on predefined mapping rules.
+
+    ✅ No dependencies required — pure Node.js.
---
+++ index.js
@@ -0,0 +1,142 @@
+import { charMap, digitToLetter } from './constants.js';
+
+// nomor 1
+function convertStringToNumber(string) {
+  let stringArr = string.split('');
+
+  return stringArr.map((char) => charMap[char]);
+}
+
+console.log(convertStringToNumber('Titanic'));
+console.log(convertStringToNumber('Avenger Endgame'));
+
+// nomor 2
+function alternateSum(numbers) {
+  let sum = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (i % 2 === 1) {
+      sum += numbers[i];
+    } else {
+      sum -= numbers[i];
+    }
+  }
+
+  return sum;
+}
+
+console.log(alternateSum(convertStringToNumber('Titanic')));
+console.log(alternateSum(convertStringToNumber('Avenger Endgame')));
+
+// nomor 3
+function convertNumberToLetter(number) {
+  const absNumber = Math.abs(number);
+  const digits = [];
+
+  let remaining = absNumber;
+
+  const cycle = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let i = 0;
+
+  while (remaining > 0) {
+    const digit = cycle[i % cycle.length];
+
+    if (digit <= remaining) {
+      digits.push(digit);
+      remaining -= digit;
+      i++;
+    } else {
+      i = -1;
+      i++;
+    }
+  }
+
+  return digits.map((d) => digitToLetter[d]).join('');
+}
+
+console.log(
+  convertNumberToLetter(alternateSum(convertStringToNumber('Titanic')))
+);
+console.log(
+  convertNumberToLetter(alternateSum(convertStringToNumber('Avenger Endgame')))
+);
+
+// nomor 4
+function convertLettersToOtherLetter(letters) {
+  const letterArr = letters.split('');
+
+  // Step 1: Convert letters to digits
+  const digits = letterArr.map((char) => charMap[char]);
+
+  /**
+   * step 2:
+   *
+   * -  take last 2 digits,
+   * -  assume 0 as x, 1 as y
+   * -  replace x with y and y with y + 1
+   * -  rejoin
+   */
+
+  const lastTwoDigits = digits.slice(-2);
+
+  const y = lastTwoDigits[1];
+
+  lastTwoDigits[0] = y;
+  lastTwoDigits[1] = y + 1;
+
+  return digits
+    .slice(0, digits.length - 2)
+    .concat(lastTwoDigits)
+    .map((d) => digitToLetter[d])
+    .join('');
+}
+
+console.log(
+  convertLettersToOtherLetter(
+    convertNumberToLetter(alternateSum(convertStringToNumber('Titanic')))
+  )
+);
+
+console.log(
+  convertLettersToOtherLetter(
+    convertNumberToLetter(
+      alternateSum(convertStringToNumber('Avenger Endgame'))
+    )
+  )
+);
+
+// nomor 5
+function convertLettersToDigits(letters) {
+  const letterArr = letters.split('');
+
+  // Step 1: Convert letters to digits
+  const digits = letterArr.map((char) => charMap[char]);
+
+  /**
+   * Step 2: assumption
+   * even → digit + 1, odd → same
+   */
+  const transformed = digits.map((digit) =>
+    digit % 2 === 0 ? digit + 1 : digit
+  );
+
+  return transformed;
+}
+
+console.log(
+  convertLettersToDigits(
+    convertLettersToOtherLetter(
+      convertNumberToLetter(alternateSum(convertStringToNumber('Titanic')))
+    )
+  )
+);
+
+console.log(
+  convertLettersToDigits(
+    convertLettersToOtherLetter(
+      convertNumberToLetter(
+        alternateSum(convertStringToNumber('Avenger Endgame'))
+      )
+    )
+  )
+);
